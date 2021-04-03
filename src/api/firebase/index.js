7be013d9c47c92ec0ac731fcfd91aa357d918d85
Carlos <carlosgarcia_97gr@hotmail.com>
2021-04-02 20:53:28 -0500

    add config firebase

--- src/api/firebase/index.js
+++ src/api/firebase/index.js
@@ -1,5 +1,9 @@
 const firebaseConfig = {
-    id:"random"
-}
-
+    apiKey: "AIzaSyCwhxy2PN4t4HXUgGVwNl5ifoTgFi2MTtA",
+    authDomain: "user-managment-48f2c.firebaseapp.com",
+    projectId: "user-managment-48f2c",
+    storageBucket: "user-managment-48f2c.appspot.com",
+    messagingSenderId: "910740936949",
+    appId: "1:910740936949:web:ce61e6478b4ab001bf0dbc"
+  };
 export default firebaseConfig;
